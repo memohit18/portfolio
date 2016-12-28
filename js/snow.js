f65302133429fe35c5f7e33a6b5c8136c8354128
mohi2010sm <mohit2010sm@gmail.com>
2016-12-28 22:51:26 +0530

    updated

--- js/snow.js
+++ js/snow.js
@@ -1,1 +1,19 @@
-bla
+
+var myImage=document.getElementById("img");
+
+var imageArray=["building.jpg","dog.jpg","cat.jpg","sea.jpg"];
+
+var imageIndex=0;
+
+function ChangeImage(){
+	img.attribute("src" ,imageArray[imageIndex]);
+	imageIndex++;
+	if(imageIndex>=imageArray.length){
+		imageIndex=0;}
+     }
+	 
+var intervalHandle=setInterval(ChangeImage,2000);
+
+img.onClick=function(){
+	clearInterval(intervalHandle);}
+	
